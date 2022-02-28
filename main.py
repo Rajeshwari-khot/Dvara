1889c1ab15897673e139d2751d45a3a41d8b7a10
unknown <rajeshwarikhot11@gmail.com>
2022-02-28 11:50:11 +0530

    few api's added

--- main.py
+++ main.py
@@ -10,14 +10,10 @@ async def root():
     response=json.load(data)
     return response
 
-@app.get("/customerdata")
+@app.post("/customerdata")
 async def customer_data():
-    json_data=await root()
-    return (json_data['enrollmentDTO']['customer'])
-   
-@app.get("/user")
-async def user_data():
-    json_data=await customer_data()
+    customer_request=await root()
+    json_data=(customer_request['enrollmentDTO']['customer'])
     id =(json_data["id"])
     firstname=(json_data["firstName"])
     lastname=(json_data["lastName"])
@@ -37,7 +33,9 @@ async def user_data():
     street=(json_data['mailingStreet'])
     post=(json_data['mailingPostoffice'])
     local=(json_data['mailingLocality'])
-    bankname=(json_data[''])
+    
+    
+   
     # Year=(json_data['year'])
     # Month=(json_data['month'])
     # Value=(json_data['monthValue'])
@@ -50,12 +48,31 @@ async def user_data():
 
     final=(firstname+ str(middlename)+str(lastname))
     addr=(doorno+str(street)+str(post))
-    # sample=(Year+str(Month)+str(Value)+str(day)+str( week)+str(era)+str(dayyear)+str(leap))
+    
+    data=(customer_request['enrollmentDTO']['customer']['customerBankAccounts'])
+    for ln in data:
+       
+      bankname=(ln['customerBankName'])
+      accno=(ln['accountNumber'])
+      ifsccode=(ln['ifscCode'])
+      acctype=(ln['accountType'])
+    date_data=(customer_request['enrollmentDTO']['customer']['dateOfBirth'])
+    for ln in [date_data]:
+       
+        Year=(ln['year'])
+        Month=(ln['month'])
+        Value=(ln['monthValue'])
+        day=(ln['dayOfMonth'])
+        week=(ln['dayOfWeek'])
+        era=(ln['era'])
+        dayyear=(ln['dayOfYear'])
+        leap=(ln['leapYear'])
+        sample=(str(Year)+str(Month)+str(Value)+str(day)+str( week)+str(era)+str(dayyear)+str(leap))
     
 
     if lastname or middlename is None:
          if LastName or MiddleName is None:
-             if street or post is "":
+             if street or post is " ":
               return {
              "sm_user_id" : id,
              "name":firstname,
@@ -63,30 +80,51 @@ async def user_data():
              "marital_status":maritalstatus,
               "mobile_number":mobileno,
               "email_id":email,
-               "res_address":doorno+local,
+            "res_address":doorno+local,
               "res_city":city,
               "res_state":state,
               "res_pin_code":pincode,
               "pan_number":panno,
-            #   "date_of_birth":Year-Value-day,
+              "bank_name":bankname,
+              "account_number":accno,
+              "ifsc_code":ifsccode,
+              "account_type":acctype,
+              "date_of_birth":sample
+            
              
 
               }
 @app.get("/loandata")
 async def loan_data():
-    json_data=await root()
-    return (json_data['loanDTO']['loanAccount'])
-
-@app.get("/loan_request")
-async def loan_request():
-    json_data=await loan_data()
+    loan_request=await root()
+    json_data=(loan_request['loanDTO']['loanAccount'])
     loanamount=(json_data['loanAmount'])
     interest=(json_data['interestRate'])
+   
     tenure=(json_data['tenure'])
+    processfee=(json_data['processingFeeInPaisa'])
+    
+    insurance=(json_data['insuranceFee'])
+    additional=(json_data['otherFee'])
+    none='not found'
+    loan=(loan_request['loanDTO']['loanAccount']['disbursementSchedules'])
+    for ln in loan:
+       d_amount=(ln['disbursementAmount'])
     return {
         "loan_amount":loanamount,
         "interest_rate":interest,
+        "disbursement_amount":d_amount,
         "tenure":tenure,
+        "repayment_schedule_json":none,
+        "am_user_token":none,
+        "sm_user_id":none,
+        "sm_loan_id":none,
+        "processing_fee":processfee,
+         "additional_charges":additional,
+        "insurance_charges":insurance,
+        "number_of_edis":none
+        
+
 
 
     }
